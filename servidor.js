0a0301ae4850edf5f85e75628d857c7e700cf1c5
Sabrina <sabrinamayume2014@gmail.com>
2025-06-02 15:25:32 -0300

    postar, listar e ver média da forma certa usando ejs, servidor, e mongodb

--- servidor.js
+++ servidor.js
@@ -1,50 +1,70 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-app.use(express.static('./public'));
 const bodyparser = require('body-parser');
+var mongodb = require("mongodb");
+
+app.use(express.static('./public'));
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 var server = http.createServer(app);
 server.listen(80);
 console.log('Servidor rodando...');
-var mongodb = require("mongodb");
+
 const MongoClient = mongodb.MongoClient;
 const uri = "mongodb+srv://sabrina:may123@cluster0.tlphc9u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect()
+client.connect().then(() => {
     const dbo = client.db("alunos");
     const colecao = dbo.collection("notas");
 
     app.post('/cadastrar', function (req, res) {
-        console.log("Dados recebidos:", req.body);
+        const { nome, nota1, nota2, nota3 } = req.body;
+        const dados = { nome, nota1, nota2, nota3 };
 
-        const aluno = req.body.nome;
-        const nota1 = req.body.nota1;
-        const nota2 = req.body.nota2;
-        const nota3 = req.body.nota3;
-
-        const dados = {
-            nome : aluno,
-            nota1 : nota1,
-            nota2 : nota2,
-            nota3 : nota3
-        };
         colecao.insertOne(dados, function (err) {
             if (err) {
-                res.redirect('/resposta_media_erro.html');
+                res.render('resposta_media_erro', { nome });
             } else {
-                res.redirect('/resposta_media_sucesso.html');
+                res.redirect('/pagina'); // redireciona para listagem
             }
         });
     });
+
     app.get('/cadastrar', function (req, res) {
-    colecao.find({}).toArray(function (err, result) {
-        if (err) {
-            res.redirect('/resposta_media_erro.html');
-        } else {
-            res.status(200).json(result); // envia os dados como JSON
-        }
+        colecao.find({}).toArray(function (err, result) {
+            if (err) {
+                res.status(500).json({ erro: 'Erro ao buscar dados' });
+            } else {
+                res.status(200).json(result);
+            }
+        });
     });
+
+    app.get('/pagina', function (req, res) {
+        res.render('pagina');
+    });
+
+    app.get('/media', function (req, res) {
+        const nomeAluno = req.query.aluno_media;
+
+        colecao.findOne({ nome: nomeAluno }, (err, usuario) => {
+            if (err || !usuario) {
+                res.render('resposta_media_erro', { nome: nomeAluno });
+            } else {
+                const nota1 = parseFloat(usuario.nota1);
+                const nota2 = parseFloat(usuario.nota2);
+                const nota3 = parseFloat(usuario.nota3);
+                const media = ((nota1 + nota2 + nota3) / 3).toFixed(2);
+
+                res.render('resposta_media_sucesso', { nome: nomeAluno, media });
+            }
+        });
+    });
+
 });
